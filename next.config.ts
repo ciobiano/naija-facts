4199917019d47d6ca2043f9d0cc8b4cd3440c43f
ciobiano <rafaelobiano@yahoo.com>
2025-05-15 21:12:58 +0100

    Refactor: code structure for improved readability and maintainability

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.freepik.com/**",
-      },
-    ],
-  },
-  // if used turbopack
-  // transpilePackages: ["next-mdx-remote"],
+	/* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "img.freepik.com/**",
+			},
+		],
+	},
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.module.rules.push({
+				test: /\.node$/,
+				use: "node-loader",
+			});
+		}
+		return config;
+	},
 };
 
 export default nextConfig;
